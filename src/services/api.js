1cb2b8bdb0dba972ed77ae4a6ec323f66344b47b
muhammad-ghopur <parantinuyul9@gmail.com>
2025-07-19 00:06:45 +0700

    Update BASE_URL ke backend baru

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'https://228f4a29-9dec-4b73-90e6-428879825d53-00-19xgx8u9ma502.pike.replit.dev';
 
 // Helper function untuk membuat request
 const makeRequest = async (endpoint, options = {}, isFormData = false) => {
